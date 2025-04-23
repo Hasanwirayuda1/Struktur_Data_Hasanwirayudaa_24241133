1294f2323beb7f9839a52254281eda6cd482724e
Hasanwirayuda1 <hasandaa.15@gmail.com>
2025-04-24 05:51:22 +0800

    Add files via upload

---
+++ tugas10(24241133).py
@@ -0,0 +1,38 @@
+# perbandingan - jawaban ada 2 true false
+
+# >,  <, >=,  <=, ==,  !=, is, is not
+
+x = 24
+y = 33
+
+print("======lebih dari (>)======")
+hasil = x > 33
+print("x > 33 = ", hasil)
+
+print("======kurang dari (<)======")
+hasil = x < 33
+print("x < 33 = ", hasil)
+
+print("======lebih dari sama dengan (>=)======")
+hasil = x >= y
+print(x,">=",y ,"=",hasil)
+
+print("======kurang dari sama dengan (<=)======")
+hasil = x <= y
+print(x,"<=",y ,"=",hasil)
+
+print("======sama dengan sama dengan (==)======")
+hasil = x == y
+print(x,"==",y ,"=",hasil)
+
+print("======tidak sama dengan (!=)======")
+hasil = x != y
+print(x,"!=",y ,"=",hasil)
+
+print("======(is)======")
+hasil = x is y
+print(x,"is",y ,"=",hasil)
+
+print("======(is not)======")
+hasil = x is not y
+print(x,"is not",y ,"=",hasil)
