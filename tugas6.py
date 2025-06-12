13f0e98e9d848c0cbde0d56f56de4095f8d1773f
Hasanwirayuda1 <hasandaa.15@gmail.com>
2025-06-13 06:25:02 +0800

    Add files via upload

---
+++ tugas6.py
@@ -0,0 +1,15 @@
+number = 1234567890
+number = str(number)
+
+#menampilkan data terakhir
+print("a) data terakhir:", number[-1])
+
+#menampilkan data pertama
+print("b) data pertama:", number[0])
+
+#menampilkan data 3-6
+print("c) data ke 3 sampai ke 6 :", number[2:6])
+
+
+
+
